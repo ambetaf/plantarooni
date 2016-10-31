4c13ff3fdc72d031ca261109abd5120b2d65de2b
Penguin <starjirachi1@yahoo.com>
2016-10-31 18:13:35 +0800

    Added feature of changing system settings

---
+++ app/controllers/system_settings_controller.rb
@@ -0,0 +1,20 @@
+class SystemSettingsController < ApplicationController
+  def configuration
+    @moisture_threshold = SystemSettings.instance.moisture_threshold
+  end
+
+  def update
+    @moisture_threshold = SystemSettings.instance.moisture_threshold
+    if SystemSettings.instance.update(settings_params) # if success
+      redirect_to system_settings_configuration_path
+    else # if not
+      render :configuration
+    end
+  end
+
+  private
+
+  def settings_params
+    params.permit(:moisture_threshold)
+  end
+end
--- db/migrate/20160913062040_create_moisture_sensor_readings.rb
+++ db/migrate/20160913062040_create_moisture_sensor_readings.rb
@@ -1,7 +1,6 @@
 class CreateMoistureSensorReadings < ActiveRecord::Migration[5.0]
   def change
     create_table :moisture_sensor_readings do |t|
-
       t.integer :measurement
       t.timestamps
     end
