51a3f0930ac6b17b10dd97fb39e9b454bc9ee882
Albert A. Fuensalida Jr <ambetaf@gmail.com>
2016-11-24 18:54:24 +0800

    Authentication added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root :to => 'users#index'
+  resources :user_sessions
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/moisture_readings', to: 'moisture_sensor_readings#index', as: 'moisture_readings'
   get '/temperature_readings', to: 'temperature_sensor_readings#index', as: 'temperature_readings'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110060128) do
+ActiveRecord::Schema.define(version: 20161117062434) do
 
   create_table "moisture_sensor_readings", force: :cascade do |t|
     t.integer  "measurement"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20161110060128) do
     t.float    "measurement"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",         null: false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
