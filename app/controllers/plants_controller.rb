61aa90f6357106d10c7be4e208a0d07e0459f826
Christley Johann Ho <reiden4@yahoo.com>
2016-12-01 00:18:44 +0800

    Plant logic

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -2,4 +2,10 @@ class PlantsController < ApplicationController
   def index
     @plants = Plant.all
   end
+
+  def update_threshold
+    plant = Plant.find(params[:id])
+    SystemSettings.instance.update(humidity_threshold: plant.humidity_threshold, moisture_threshold: plant.moisture_threshold, temperature_threshold: plant.temperature_threshold)
+  end
+
 end
