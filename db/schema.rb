9d667b039013016f247d060fa563590523b049d8
Albert A. Fuensalida Jr <ambetaf@gmail.com>
2016-12-04 22:37:14 +0800

    Added Humidity on SystemSettigns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129085700) do
+ActiveRecord::Schema.define(version: 20161204124827) do
 
   create_table "Plants", force: :cascade do |t|
     t.string   "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20161129085700) do
     t.float    "temperature_threshold"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
+    t.integer  "humidity_threshold"
   end
 
   create_table "temperature_sensor_readings", force: :cascade do |t|
