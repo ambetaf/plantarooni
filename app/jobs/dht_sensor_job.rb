57307de73b7228f2c0ead7d178d064e534d1163d
Penguin <starjirachi1@yahoo.com>
2016-12-10 15:39:50 +0800

    try b bert

--- app/jobs/dht_sensor_job.rb
+++ app/jobs/dht_sensor_job.rb
@@ -1,16 +1,17 @@
 class DhtSensorJob
   include SuckerPunch::Job
 
-  def perform(cooling_fan, exhaust_fan)
+  def perform
     while true
+      puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
       val = DhtSensor.read(4, 11)
       TemperatureSensorReading.create(measurement: val.temp)
       HumiditySensorReading.create(measurement: val.humidity)
       unless SystemSettings.instance.manual_control
-        cooling_fan.send(val.temp > SystemSettings.instance.temperature_threshold ? :on : :off)
-        exhaust_fan.send(val.humidity > SystemSettings.instance.humidity_threshold ? :on : :off)
+        Board.cooling_fan.send(val.temp > SystemSettings.instance.temperature_threshold ? :on : :off)
+        Board.exhaust_fan.send(val.humidity > SystemSettings.instance.humidity_threshold ? :on : :off)
       end
-      sleep 300
+      sleep 1
     end
   end
 end
