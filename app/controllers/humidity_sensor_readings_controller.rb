151ddae73a8b17bcbddfdb2fae25e211968eaca5
Aaron <aaronlim023@yahoo.com>
2016-12-10 08:20:30 +0800

    changed layout for displaying of data

--- app/controllers/humidity_sensor_readings_controller.rb
+++ app/controllers/humidity_sensor_readings_controller.rb
@@ -1,5 +1,5 @@
 class HumiditySensorReadingsController < ApplicationController
   def index
-    @readings = HumiditySensorReading.order(:created_at).reverse
+    @readings = HumiditySensorReading.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
   end
 end
