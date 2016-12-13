13eea23058e0e7bfc21e55852a08efc163c451ee
Penguin <starjirachi1@yahoo.com>
2016-12-14 02:37:50 +0800

    yay

--- app/jobs/moisture_sensor_job.rb
+++ app/jobs/moisture_sensor_job.rb
@@ -1,12 +1,12 @@
 class MoistureSensorJob
   include SuckerPunch::Job
+  @moisture_sensor_time = Time.now
 
-moisture_sensor_time = Time.now
   def perform
     Board.moisture_sensor.when_data_received do |data|
-      if Time.now - moisture_sensor_time > 5.minutes
+      if Time.now - @moisture_sensor_time > 5.minutes
         MoistureSensorReading.create(measurement: data)
-        moisture_sensor_time = Time.now
+        @moisture_sensor_time = Time.now
         Board.sprinkler.send(data.to_i < SystemSettings.instance.moisture_threshold ? :on : :off) unless SystemSettings.instance.manual_control
       end
     end
